bf8a3eb597918792f608b554be5318bab86754f1
githubindia <shubham08gupta08@gmail.com>
2018-04-25 18:24:25 +0530

    requiring deasync done

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var serviceNow = require('./servicenow.js');
 const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAHboQmC66s33wksVxCsAjOZAr5scCnsEFc0P2IrFrOvEO9jip3rjoZBo0PDzTckZAWVPwOZC9POI8GldBEALmpP6q8NTeU4ZA0XIp7ZB96gj0rqcSfYR3HQ6Ue3oTmBUNA6Q6lhELpNmtZAj3ttn23lIXh16kTeqQZDZD';
+var deasync = require('deasync');
 
 module.exports = {
     "getToken": function (request, response) {
