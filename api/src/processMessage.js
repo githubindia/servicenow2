89ec5bf8fc98667448c349503f4619624d651cf2
githubindia <shubham08gupta08@gmail.com>
2018-04-25 12:59:24 +0530

    response changed

--- api/src/processMessage.js
+++ api/src/processMessage.js
@@ -10,7 +10,7 @@ const sendTextMessage = (senderId, res) => {
         method: 'POST',
         json: {
             recipient: { id: senderId },
-            message: { res },
+            message: { "text":"hello" },
         }
     }, (err, res, body) => {
         if (!err) {
