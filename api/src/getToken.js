57a2cd47997e17548302c0b0c3948036283aebfa
githubindia <shubham08gupta08@gmail.com>
2018-04-25 16:35:29 +0530

    flow changed

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -2,10 +2,13 @@ var path = require('path');
 
 module.exports = {
     "getToken": function (request, response) {
-        response.redirect('/webhook/close');
+        // After getting token redirect to specific URL
+        // response.redirect('/webhook/close');1
+        response.redirect('https://www.messenger.com/closeWindow/?display_text=Authenticated');
     },
     "getUser": function (request, response) {
         console.log(request.query.psid);
+        response.redirect('/webhook/auth/provider');
         // response.redirect('https://www.messenger.com/closeWindow/?display_text=Authenticated');
     }
 
