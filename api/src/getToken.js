cec9c1f7ae3b2665a47ef56c6e2dc7fc9c1d1a54
githubindia <shubham08gupta08@gmail.com>
2018-04-25 15:51:49 +0530

    path corrected

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     "getToken": function (request, response) {
-        response.redirect((path.resolve(__dirname + '/closeWindow.html')));
+        response.redirect((path.resolve(__dirname + '../../closeWindow.html')));
     },
     "getUser": function (request, response) {
         console.log(req.query.psid);
