4e0a70bf031b6f8000d10fe88f1a6489231f669e
githubindia <shubham08gupta08@gmail.com>
2018-04-25 12:53:59 +0530

    token changed

--- api/src/processMessage.js
+++ api/src/processMessage.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAMZCh6VJ7ZAsEfXE5sk62YhBoMUinTYArAY7qscG8hgZBJ2xsH1a9BnXXu4BXBWyjZANCaoapGRV0qQCLTZABi8ZArlg4YZAdoFw8MlhdhpNcrUCHE2ZCSbjRCcpFrczW54PAwO1VIjr30wvU4P7rlYdN8RI9tBSoQZDZD';
+const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAHboQmC66s33wksVxCsAjOZAr5scCnsEFc0P2IrFrOvEO9jip3rjoZBo0PDzTckZAWVPwOZC9POI8GldBEALmpP6q8NTeU4ZA0XIp7ZB96gj0rqcSfYR3HQ6Ue3oTmBUNA6Q6lhELpNmtZAj3ttn23lIXh16kTeqQZDZD';
 const API_AI_TOKEN = 'd786ef841ad7471a978e218ca532df82'; // silly-name-maker agent.
 const apiAiClient = require('apiai')(API_AI_TOKEN);
 
@@ -61,3 +61,4 @@ module.exports = (event) => {
     apiaiSession.on('error', error => console.log(error));
     apiaiSession.end();
 };
+EAAc6hI7VvPwBAMZCh6VJ7ZAsEfXE5sk62YhBoMUinTYArAY7qscG8hgZBJ2xsH1a9BnXXu4BXBWyjZANCaoapGRV0qQCLTZABi8ZArlg4YZAdoFw8MlhdhpNcrUCHE2ZCSbjRCcpFrczW54PAwO1VIjr30wvU4P7rlYdN8RI9tBSoQZDZD
