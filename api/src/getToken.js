f1b8ad1c81d1591a90c386125249b909f36711fd
githubindia <shubham08gupta08@gmail.com>
2018-04-25 15:48:41 +0530

    path added

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -1,6 +1,8 @@
+var path = require('path');
+
 module.exports = {
     "getToken": function (request, response) {
-        response.redirect('/closeWindow.html');
+        response.redirect((path.resolve(__dirname + '/closeWindow.html')));
     },
     "getUser": function (request, response) {
         console.log(req.query.psid);
