fa476fd8a5b21f9ca679880214bd19ee58beab40
githubindia <shubham08gupta08@gmail.com>
2018-04-25 18:41:26 +0530

    logs added

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -7,7 +7,8 @@ module.exports = {
     "getToken": function (request, response) {
         // After getting token redirect to specific URL
         // response.redirect('/webhook/close');1
-        response.send('https://www.messenger.com/closeWindow/?display_text=Authenticated');
+        // response.send('https://www.messenger.com/closeWindow/?display_text=Authenticated');
+        response.status(200).send('Please close this window to return to the conversation thread.');
         console.log(request.session.senderId);
         console.log(request.session.passport.user.accessToken);
         var psid = request.session.senderId;
