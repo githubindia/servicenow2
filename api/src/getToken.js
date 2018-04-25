83719c05d1c9298608372bbfc08f12c7758f2e44
githubindia <shubham08gupta08@gmail.com>
2018-04-25 18:52:09 +0530

    response changed

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -2,6 +2,7 @@ var path = require('path');
 var serviceNow = require('./servicenow.js');
 const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAHboQmC66s33wksVxCsAjOZAr5scCnsEFc0P2IrFrOvEO9jip3rjoZBo0PDzTckZAWVPwOZC9POI8GldBEALmpP6q8NTeU4ZA0XIp7ZB96gj0rqcSfYR3HQ6Ue3oTmBUNA6Q6lhELpNmtZAj3ttn23lIXh16kTeqQZDZD';
 var deasync = require('deasync');
+var request = require('request');
 
 module.exports = {
     "getToken": function (request, response) {
@@ -17,26 +18,26 @@ module.exports = {
         let serviceNowResponse;
         //let serviceNowResponse = deasync(function(callback){
             serviceNow.logIncident(token, function(err, body){
-                console.log(body);
+                serviceNowResponse = body;
+                result = `Your incident has been created with the incident number ${serviceNowResponse.result.number}.`
+                request({
+                    url: 'https://graph.facebook.com/v2.6/me/messages',
+                    qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+                    method: 'POST',
+                    json: {
+                        recipient: { id: psid },
+                        message: {"text": result}
+                    }
+                }, (err, res, body) => {
+                    if (!err) {
+                        console.log('message sent!')
+                    } else {
+                        console.error("Unable to send message:" + err);
+                    }
+                });
             });
         //})();
         //console.log(serviceNowResponse);
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: FACEBOOK_ACCESS_TOKEN },
-            method: 'POST',
-            json: {
-                recipient: { id: psid },
-                message: {"text": serviceNowResponse}
-            }
-        }, (err, res, body) => {
-            if (!err) {
-                console.log('message sent!')
-            } else {
-                console.error("Unable to send message:" + err);
-            }
-    });
-
 
     },
     "getUser": function (request, response) {
