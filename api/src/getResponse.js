7191d04274ee9a56947e87e5ca1994b0c3de8cf9
githubindia <shubham08gupta08@gmail.com>
2018-04-26 16:35:13 +0530

    incident_initialized intent added

--- api/src/getResponse.js
+++ api/src/getResponse.js
@@ -10,8 +10,14 @@ module.exports = {
             callback(null, response);
         } else if (request.result.metadata.intentName == 'raiseRequest') { 
 
-         } //else if () {
-
-        // }
+        } else if (request.result.metadata.intentName == 'incident_initialized') {
+            var response;
+            request.result.fulfillment.messages.forEach(function(element){
+                if (element.type == 4){
+                    response = element.payload.facebook;
+                }
+            });
+            callback(null, response);
+        }
     }
 }
