e1866b61beabcd6304a17026acdeb1ce4328f054
githubindia <shubham08gupta08@gmail.com>
2018-04-26 15:54:43 +0530

    response corrected

--- api/src/getResponse.js
+++ api/src/getResponse.js
@@ -2,7 +2,7 @@ module.exports = {
     "makeResponse": function(request, callback) {
         if (request.result.metadata.intentName == 'Default Welcome Intent') {
             var response;
-            request.fulfillment.messages.forEach(function(element){
+            request.result.fulfillment.messages.forEach(function(element){
                 if (element.type == 4){
                     response = element.payload.facebook;
                 }
