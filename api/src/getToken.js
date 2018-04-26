097489afb5ca2e4982b04364e13bb4670eed425d
githubindia <shubham08gupta08@gmail.com>
2018-04-26 20:38:08 +0530

    global modified

--- api/src/getToken.js
+++ api/src/getToken.js
@@ -3,6 +3,7 @@ var serviceNow = require('./servicenow.js');
 const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAHboQmC66s33wksVxCsAjOZAr5scCnsEFc0P2IrFrOvEO9jip3rjoZBo0PDzTckZAWVPwOZC9POI8GldBEALmpP6q8NTeU4ZA0XIp7ZB96gj0rqcSfYR3HQ6Ue3oTmBUNA6Q6lhELpNmtZAj3ttn23lIXh16kTeqQZDZD';
 var deasync = require('deasync');
 var request1 = require('request');
+global.session = [];
 
 module.exports = {
     // After getting token this method called.
@@ -15,8 +16,11 @@ module.exports = {
         console.log(request.session.passport.user.accessToken);
         var psid = request.session.senderId;
         var token = request.session.passport.user.accessToken;
-        global.session.storeToken.psid = psid;
-        global.session.storeToken.token = token;
+        // global.session.storeToken.psid = psid;
+        // global.session.storeToken.token = token;
+        global.session.push({
+            "token":token
+        })
         //console.log("done");
         let serviceNowResponse;
         var userName;
