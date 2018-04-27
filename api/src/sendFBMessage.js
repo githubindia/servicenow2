e9db6a7eb5c34be50fe9330b6397607302b355a8
githubindia <shubham08gupta08@gmail.com>
2018-04-27 13:10:05 +0530

    token added

--- api/src/sendFBMessage.js
+++ api/src/sendFBMessage.js
@@ -1,4 +1,6 @@
 var request = require('request');
+const FACEBOOK_ACCESS_TOKEN = 'EAAc6hI7VvPwBAHboQmC66s33wksVxCsAjOZAr5scCnsEFc0P2IrFrOvEO9jip3rjoZBo0PDzTckZAWVPwOZC9POI8GldBEALmpP6q8NTeU4ZA0XIp7ZB96gj0rqcSfYR3HQ6Ue3oTmBUNA6Q6lhELpNmtZAj3ttn23lIXh16kTeqQZDZD';
+
 module.exports = {
     "sendResponse": function (psid, response, callback) {
         request({
