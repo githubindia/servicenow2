baef3aae6d48ed58aace5b26ad71b1c2cda97c88
githubindia <shubham08gupta08@gmail.com>
2018-05-02 16:56:33 +0530

    env added

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ config.instanceURL = process.env.INSTANCE_URL;
 
 // OAuth Configuration 
 config.oauth = {};
-config.oauth.clientID = SN_CLIENT_ID;
-config.oauth.clientSecret = SN_CLIENT_SECRET;
+config.oauth.clientID = process.env.SN_CLIENT_ID;
+config.oauth.clientSecret = process.env.SN_CLIENT_SECRET;
 config.oauth.authURL = config.instanceURL + '/oauth_auth.do';
 config.oauth.tokenURL = config.instanceURL + '/oauth_token.do';
 config.oauth.callbackURL = 'https://servicenow2.herokuapp.com/webhook/auth/provider/callback';
