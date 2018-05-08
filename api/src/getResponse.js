a48989a5d3388d406a3a00171e08b871479f2d44
githubindia <shubham08gupta08@gmail.com>
2018-05-08 15:04:43 +0530

    token added

--- api/src/getResponse.js
+++ api/src/getResponse.js
@@ -524,7 +524,7 @@ module.exports = {
                             serviceNow.softwareInstallRequest(sysId, element.token, function(err, body) {
                                 var reqNumber;
                                 if(body.error == undefined) {
-                                    serviceNow.checkoutRequest(token, function(err, body2){
+                                    serviceNow.checkoutRequest(element.token, function(err, body2){
                                         console.log(body2);
                                         reqNumber = body2.result.request_number;
                                     })
